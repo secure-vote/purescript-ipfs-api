213c6f47b5ec583cb78fbc276c5825072bf30acb
Max Kaye <m@xk.io>
2018-06-27 14:13:26 +1000

    Update IPFS.js

--- src/IPFS.js
+++ src/IPFS.js
@@ -1,6 +1,8 @@
 "use strict";
 
-var ipfsAPI = require("ipfs-api");
+const ipfsAPI_ = require("ipfs-api");
+// rollup complains about calling a namespace...
+const ipfsAPI = ipfsAPI_;
 
 exports.connectImpl = function(host, port) {
     return ipfsAPI(host, port, {protocol: "http"});
